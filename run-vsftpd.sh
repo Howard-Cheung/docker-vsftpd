20c0c2c8d03a16a1c3b45f44384070462f1ae9f7
张哲魁 <lijiang.zhang@todoen.com>
2019-01-29 16:55:07 +0800

    listen port

--- run-vsftpd.sh
+++ run-vsftpd.sh
@@ -37,6 +37,10 @@ echo "pasv_enable=${PASV_ENABLE}" >> /etc/vsftpd/vsftpd.conf
 echo "file_open_mode=${FILE_OPEN_MODE}" >> /etc/vsftpd/vsftpd.conf
 echo "local_umask=${LOCAL_UMASK}" >> /etc/vsftpd/vsftpd.conf
 
+if [ $LISTEN_PORT ]; then
+    echo "listen_port=${LISTEN_PORT}" >> /etc/vsftpd/vsftpd.conf
+fi
+
 # Get log file path
 export LOG_FILE=`grep xferlog_file /etc/vsftpd/vsftpd.conf|cut -d= -f2`
 
